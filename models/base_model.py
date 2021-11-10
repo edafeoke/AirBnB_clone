a9ade096e185ab508a8cfd68d90e660d5615281a
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-09 22:45:09 -0800

    Updated models/base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@ base_model module
 
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -12,14 +13,23 @@ class BaseModel:
     defines all common attributes/methods for other classes
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''
         Instatiation method
         '''
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs):
+            for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
+                elif key == 'created_at' or key == 'updated_at':
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         '''
@@ -35,6 +45,7 @@ class BaseModel:
         '''
 
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         '''
