9ca9e55fc9bde2bd0bfac351606ae4526b91e9f8
knightiti <ktess2008@gmail.com>
2021-11-12 13:21:03 -0800

    <supporting files>

---
+++ models/amenity.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''
+amenity module
+'''
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    '''
+    Amentity class
+
+    Attribute:
+        name: string
+    '''
+
+    name = ''
