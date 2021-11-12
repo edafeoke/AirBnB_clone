0ef44d47b2e6b02752d6b49e85142687a0b7f488
knightiti <ktess2008@gmail.com>
2021-11-12 02:17:00 -0800

    <Linking basemodel to filestorage>

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ base_model module
 
 import uuid
 from datetime import datetime
-import models
+from models import storage
 
 
 class BaseModel:
@@ -15,7 +15,7 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         '''
-        Instatiation method
+        Instantiation method
         '''
 
         if len(kwargs):
@@ -23,13 +23,13 @@ class BaseModel:
                 if key == "__class__":
                     continue
                 elif key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         '''
@@ -45,7 +45,7 @@ class BaseModel:
         '''
 
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         '''
