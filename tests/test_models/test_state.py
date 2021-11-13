e99fd0304dd6b6bb26710dae07e0e1ae40c4645f
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-13 04:38:37 -0800

    Added more test files

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''
+module containing User class tests
+'''
+
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    '''
+    Test class that tests the User class
+    '''
+
