c37b776e3d8ee4cb6ae9de278f43d908b3b740d9
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-15 14:14:42 -0800

    Added console

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -19,6 +19,20 @@ class TestUser(unittest.TestCase):
         obj = User()
         self.assertIsInstance(obj, User, "")
 
+    def setUp(self):
+        '''
+        Setup test method
+        '''
+        user = User()
+        user.name = "Edafe Oke"
+        user.email = "greatedafeoke@gmail.com"
+
+    def test_attributes(self):
+        '''
+        test all attributes
+        '''
+        self.assertIs(user.name, "Edafe Oke")
+        self.assertIs(user.email, "greatedafeoke@gmail.com")
 
 if __name__ == '__main__':
-    unittest.maiin()
+    unittest.main()
