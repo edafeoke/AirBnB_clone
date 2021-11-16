ea8e44a269502cb90952302c2cb30f072e4696c0
Oke Edafe Great <greatedafeoke@gmail.com>
2021-11-15 17:20:58 -0800

    Added console test file

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,13 +4,13 @@ console test module
 '''
 
 import unittest
-import console.HBNBCommand
+from console import HBNBCommand
 
 
 class TestHBNBCommand(unittest.TestCase):
-    '''
+    """
     Test console class
-    '''
+    """
 
     def setUp(self):
         '''
@@ -19,3 +19,10 @@ class TestHBNBCommand(unittest.TestCase):
 
         with patch('sys.stdout', new=StringIO()) as f:
             HBNBCommand().onecmd("help show")
+
+    def test_quit(self):
+        """Quit command"""
+        self.assertTrue(f.onecmd("quit"))
+
+if __nmae__ == '__main__':
+    unittest.main()
